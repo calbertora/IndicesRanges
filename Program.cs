ddb031571e1acaa89a41875e945d83f07b000998
Carlos Ramirez <calbertora@gmail.com>
2020-03-14 10:56:53 -0500

    Add a matrix example

--- Program.cs
+++ Program.cs
@@ -5,6 +5,34 @@ namespace IndicesRanges
 {
     class Program
     {
+        public static void JaggedExample()
+        {
+            var jagged = new int[10][]
+            {
+                new int[10] { 0,  1, 2, 3, 4, 5, 6, 7, 8, 9},
+                new int[10] { 10,11,12,13,14,15,16,17,18,19},
+                new int[10] { 20,21,22,23,24,25,26,27,28,29},
+                new int[10] { 30,31,32,33,34,35,36,37,38,39},
+                new int[10] { 40,41,42,43,44,45,46,47,48,49},
+                new int[10] { 50,51,52,53,54,55,56,57,58,59},
+                new int[10] { 60,61,62,63,64,65,66,67,68,69},
+                new int[10] { 70,71,72,73,74,75,76,77,78,79},
+                new int[10] { 80,81,82,83,84,85,86,87,88,89},
+                new int[10] { 90,91,92,93,94,95,96,97,98,99},
+            };
+
+            var selectedRows = jagged[3..^3];
+
+            foreach (var row in selectedRows)
+            {
+                var selectedColumns = row[2..^2];
+                foreach (var cell in selectedColumns)
+                {
+                    Console.Write($"{cell}, ");
+                }
+                Console.WriteLine();
+            }
+        }
         public static void ComplexExample()
         {
             int[] numbers = Enumerable.Range(0, 100).ToArray();
@@ -32,7 +60,8 @@ namespace IndicesRanges
             Span<int> z_zero = numbers[z..^0];
             Console.WriteLine($"\t{z_end[0]}..{z_end[^1]} is the same as {z_zero[0]}..{z_zero[^1]}");
         }
-        static void Main(string[] args)
+
+        public static void FirstExamples()
         {
             string[] words = new String[]
             {
@@ -77,8 +106,13 @@ namespace IndicesRanges
             Range phrase = 6..;
             foreach(var word in words[phrase])
                 Console.WriteLine($"< {word} >");
-
-            ComplexExample();
+        }
+        static void Main(string[] args)
+        {
+            
+            //FirstExamples();
+            //ComplexExample();
+            JaggedExample();
         }
     }
 }
