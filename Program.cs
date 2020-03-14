a8439a54fbb9e60a42a13c37e9978d400f33dd04
Carlos Ramirez <calbertora@gmail.com>
2020-03-14 11:01:40 -0500

    Add a real world example

--- Program.cs
+++ Program.cs
@@ -5,6 +5,35 @@ namespace IndicesRanges
 {
     class Program
     {
+        
+        public static void AvarageExample()
+        {
+            int[] sequence = Sequence(1000);
+
+            for(int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = start..(start+10);
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:    \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = ^(start + 10)..^start;
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:  \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+
+            (int min, int max, double average) MovingAverage(int[] subSequence, Range range) =>
+                (
+                    subSequence[range].Min(),
+                    subSequence[range].Max(),
+                    subSequence[range].Average()
+                );
+
+            int[] Sequence(int count) =>
+                Enumerable.Range(0, count).Select(x => (int)(Math.Sqrt(x) * 100)).ToArray();
+        }
         public static void JaggedExample()
         {
             var jagged = new int[10][]
@@ -112,7 +141,8 @@ namespace IndicesRanges
             
             //FirstExamples();
             //ComplexExample();
-            JaggedExample();
+            //JaggedExample();
+            AvarageExample();
         }
     }
 }
